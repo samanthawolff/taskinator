b3f6910dafd7eab051e3341c0e83d66addf5b4a6
Samantha Wolff <jasonwolff@Samanthas-MacBook-Air.local>
2022-02-23 22:40:23 -0600

    adding task type

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,28 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // creat list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // creat div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
+
+   // var listItemEl = document.createElement("li");
+    //listItemEl.className = "task-item";
+   // listItemEl.textContent = taskNameInput;
+   // tasksToDoEl.appendChild(listItemEl);
 };
 
 
